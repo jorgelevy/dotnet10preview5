929e7ce0a633e6c32a7b02212ecd6f494dafb055
Jorge Levy <me@jorgelevy.com>
2025-06-17 00:25:56 +0000

    Baseline

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// List<int> values = new List<int> { 0,5,6,9,20,55,99 };
+List<int> values = new List<int> { };
+
+IEnumerable<int> result = values.WhereGreaterThan(18);
+Console.WriteLine("Values greater than 18:");
+foreach (var value in result)
+{
+    Console.WriteLine(value);
+}
+
+if (result.IsEmpty)
+{
+	Console.WriteLine("There is no elements on the list");
+}
+
+
+
+public static class Extensions
+{
+	extension(IEnumerable<int> source)
+	{
+		public IEnumerable<int> WhereGreaterThan(int threshold)
+			=> source.Where(x => x > threshold);
+
+		public bool IsEmpty
+		=> !source.Any();
+	}
+}
